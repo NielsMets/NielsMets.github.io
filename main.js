2d37a837002703dca32fc11f311db216d78d19ad
Niels Metselaar <metsniels@gmail.com>
2020-12-10 11:49:33 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuItems = document.getElementsByClassName("menu-item");
+const menuBtn = document.querySelector(".menu-btn");
+
+
+for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", function() {
+        menuBtn.checked = false;
+    });
+};
