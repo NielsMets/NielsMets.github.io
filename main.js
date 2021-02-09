8745fa7e5f0972ac4e47150b128b1d4f8c0bdf69
Niels Metselaar <metsniels@gmail.com>
2021-02-09 15:33:43 +0100

    big overhaul

--- main.js
+++
@@ -1,9 +0,0 @@
-const menuItems = document.getElementsByClassName("menu-item");
-const menuBtn = document.querySelector(".menu-btn");
-
-
-for (i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener("click", function() {
-        menuBtn.checked = false;
-    });
-};
