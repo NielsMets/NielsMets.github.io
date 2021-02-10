62cb540a180f0d79bc82871266d71fd0ab90dd4a
Niels Metselaar <metsniels@gmail.com>
2021-02-10 17:01:08 +0100

    fixed jpg issue for real this time

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const menuItems = document.getElementsByClassName("menu-item");
 const menuBtn = document.querySelector(".menu-btn");
 
-
 for (i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener("click", function() {
-        menuBtn.checked = false;
-    });
-};
+  menuItems[i].addEventListener("click", function () {
+    menuBtn.checked = false;
+  });
+}
